221a14716b8db0e2aa34f244b1f05fd386d3f15f
Miyagi REN <e205707@eve.u-ryukyu.ac.jp>
2020-12-16 23:43:46 +0900

    step2

---
+++ src/Report5.java
@@ -0,0 +1,13 @@
+public class Report5{
+    public static void main(String[] args){
+        String str = null;
+        try{
+            System.out.println(str.length());
+        }
+        catch(NullPointerException e){
+            System.out.println("NullPointerExceptionが発生しました。");
+            System.out.println(e.getMessage());
+        }
+        
+    }
+}
